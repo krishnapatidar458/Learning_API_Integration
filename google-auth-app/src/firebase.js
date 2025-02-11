b0d16f6d75f2e6c1f2d04648d8ed3cc5bf6f2354
Krishna Patidar <92305156+krishnapatidar458@users.noreply.github.com>
2025-02-11 22:58:57 +0530

    Update firebase.js
    Update some credentials.

--- google-auth-app/src/firebase.js
+++ google-auth-app/src/firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoMkr8gZ3ZXP91bJdQ44hnYvv0Nfe0yT4",
-  authDomain: "auth-app-8a148.firebaseapp.com",
-  projectId: "auth-app-8a148",
-  storageBucket: "auth-app-8a148.firebasestorage.app",
-  messagingSenderId: "874032255224",
-  appId: "1:874032255224:web:4c119fdcb9e781b0662b0d",
-  measurementId: "G-WXX2PQHQJR"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
